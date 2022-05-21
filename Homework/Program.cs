93a81186fb2f735f25efca100c1f30c0a0d86b13
Emil Huseynli <emil.huseynli.std@bhos.edu.az>
2022-05-21 18:10:16 +0400

    first commit

---
+++ Homework/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 3, 21, 27, 32, 46, 58, 78, 88, 93, 95  };
+            int enteredvalue = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == enteredvalue)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
